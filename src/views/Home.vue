62c1b198b0e6505f825984443cd08826e11e2f73
marcel-git <marcel_kuehn@gmx.de>
2021-02-20 15:28:04 +0100

    -Added view 'home'

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <News msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import News from '@/components/News.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    News
+  }
+}
+</script>
